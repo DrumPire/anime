96ad6f87829b391bae55b9f6d442f89d286536e1
DrumPire <expresproject@gmail.com>
2022-09-27 00:32:25 +0300

    added categories and anime details pages, fixed preloader.js, added categoriesData/detailData.js scripts

--- js/categoriesData.js
+++ js/categoriesData.js
@@ -1,5 +1,6 @@
-const mainData = () => {
+const categoriesData = () => {
   const preloader = document.querySelector('.preloder');
+  
   const renderGanreList = ganres => {
     const dropdownBlock = document.querySelector('.header__menu .dropdown');
 
@@ -11,12 +12,12 @@ const mainData = () => {
   };
 
   const renderAnimeList = (array, ganres) => {
-    const wrapper = document.querySelector('.product .col-lg-8');
+    const wrapper = document.querySelector('.product-page .col-lg-8');
 
     ganres.forEach(ganre => {
       const productBlock = document.createElement('div');
       const listBlock = document.createElement('div');
-      const list = array.filter(item => item.ganre === ganre);
+      const list = array.filter(item => item.tags.includes(ganre));
 
       listBlock.classList.add('row');
       productBlock.classList.add('mb-5');
@@ -101,16 +102,20 @@ const mainData = () => {
     .then((response) => response.json())
     .then((data) => {
       const ganres = new Set();
+      const ganreParams = new URLSearchParams(window.location.search).get('ganre');
 
       data.anime.forEach(item => {
         ganres.add(item.ganre);
       });
       
       renderTopAnime(data.anime.sort((a,b) => b.views - a.views).slice(0, 5));
-      renderAnimeList(data.anime, ganres);
+      if (ganreParams) {
+        renderAnimeList(data.anime, [ganreParams]);
+      } else {
+        renderAnimeList(data.anime, ganres);
+      }
       renderGanreList(ganres);
-
     });
 };
 
-mainData();
+categoriesData();
